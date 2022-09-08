414da3362ec52722c56914f535465deb8ed4a062
Ibrahim Akancay <iakancay@gmail.com>
2022-09-08 12:22:42 +0200

    exercise 3.1 is done

---
+++ Week4/homework/ex1-aggregation/index.js
@@ -0,0 +1,112 @@
+const { MongoClient } = require("mongodb");
+const csvtojson = require("csvtojson");
+
+const db = "databaseWeek4";
+const collection = "world_population";
+
+//Connection to ATLAS mongodb
+const main = async () => {
+  const uri =
+    "mongodb+srv://hyfuser:hyfpassword@cluster0.6enaegs.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+    await insertData(client);
+    await calculatePopulationCountryGroupByYears(client, "Turkey");
+    await calculatePopulationContinentsForAgeAndYear(client, "100+", "2020");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }
+};
+
+const insertData = async (client) => {
+  const jsonData = await csvtojson().fromFile(
+    "population_pyramid_1950-2022.csv"
+  );
+  const result = await client
+    .db(db)
+    .collection(collection)
+    .insertMany(jsonData);
+  console.log(`${result.insertedCount} documents were added to database`);
+};
+
+const calculatePopulationCountryGroupByYears = async (client, country) => {
+  const agg = [
+    {
+      $match: {
+        Country: country,
+      },
+    },
+    {
+      $group: {
+        _id: "$Year",
+        countPopulation: {
+          $sum: {
+            $add: [
+              {
+                $toInt: "$M",
+              },
+              {
+                $toInt: "$F",
+              },
+            ],
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        _id: 1,
+      },
+    },
+  ];
+
+  const cursor = await client.db(db).collection(collection).aggregate(agg);
+  const results = await cursor.toArray();
+  results.forEach((result) => console.log(result));
+};
+const calculatePopulationContinentsForAgeAndYear = async (
+  client,
+  age,
+  year
+) => {
+  const agg = [
+    {
+      $match: {
+        Country: {
+          $in: [
+            "AFRICA",
+            "ASIA",
+            "EUROPE",
+            "LATIN AMERICA AND THE CARIBBEAN",
+            "NORTHERN AMERICA",
+            "OCEANIA",
+          ],
+        },
+        Year: year,
+        Age: age,
+      },
+    },
+    {
+      $addFields: {
+        TotalPopulation: {
+          $add: [
+            {
+              $toInt: "$M",
+            },
+            {
+              $toInt: "$F",
+            },
+          ],
+        },
+      },
+    },
+  ];
+
+  const cursor = await client.db(db).collection(collection).aggregate(agg);
+  const results = await cursor.toArray();
+  results.forEach((result) => console.log(result));
+};
+
+main().catch((err) => console.log(err));
